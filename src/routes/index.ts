6ae842c59dc7b48007ead3c3d6e523f2cc3bcf48
Spencer Hepworth <goldenspencerh@gmail.com>
2024-05-20 18:46:20 -0600

    making sure app is up to date

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,4 @@
 import { Router } from 'express';
-const { requiresAuth } = require('express-openid-connect');
 import createRoutes from '../utils/createRoutes';
 import * as UserConroller from '../controllers/UserController';
 import * as FieldController from '../controllers/FieldController';
@@ -8,16 +7,8 @@ import * as IrrigationController from '../controllers/IrrigationController';
 import { getUserFieldsDetails } from '../controllers/UserController';
 const routes = Router();
 
-routes.use(requiresAuth());
 
 
-// Auth routes
-routes.get('/', (req: any, res) => {
-    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-  });
-routes.get('/profile', (req: any, res: any) => {
-    res.send(req.oidc.user ? JSON.stringify(req.oidc.user) : 'No user info');
-});
 
 // User routes
 routes.use('/user', createRoutes(UserConroller, 'user'));
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
 console.log('BASE_URL', process.env.BASE_URL)
 import express, { Express } from 'express';
 import routes from './routes';
-import connectDatabase from './config/database';
-import authConfig from './config/auth';
-const {auth} = require('express-openid-connect'); 
+const cors = require('cors');
 
 const app: Express = express();
+app.use(cors());
 app.use(express.json());
-app.use(auth(authConfig));
 app.use('/', routes);
 
 console.log()
