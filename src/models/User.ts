db58f8f50487061680210685c0fe5a8e67bb35bd
Spencer Hepworth <118311110+recnepspencer@users.noreply.github.com>
2024-05-21 08:22:02 -0600

    Update User.ts

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,7 +1,6 @@
 import { Schema, model } from 'mongoose';
 
 const userSchema = new Schema({
-    auth0Id: { type: String, required: true, unique: true }, 
     username: { type: String, required: true },
     email: { type: String, required: true },
     fields: [{ type: Schema.Types.ObjectId, ref: 'Field' }]
