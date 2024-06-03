d00b6b47a96755ec5a9c7ae448783eda9129bd9a
Spencer Hepworth <118311110+recnepspencer@users.noreply.github.com>
2024-06-03 10:48:16 -0600

    fixed env variables

--- src/config/authConfig.ts
+++ src/config/authConfig.ts
@@ -2,7 +2,7 @@ const authConfig = {
   authRequired: process.env.AUTH_REQUIRED === 'false',
   auth0Logout: process.env.AUTH0_LOGOUT === 'true',
   secret: process.env.SECRET!,
-  baseURL: process.env.BASE_URL!,
+  baseURL: process.env.ENVIRONMENT === 'local' ? process.env.BASE_URL! : process.env.SERVER_BASE_URL!,
   clientID: process.env.CLIENT_ID!,
   issuerBaseURL: process.env.ISSUER_BASE_URL!,
 };
