65aef63e3f51c7bb78823679577176fc4ba1e6b5
Jake Zalesny <jrzalesny@gmail.com>
2024-07-18 10:46:39 -0600

    fixed bad route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -32,7 +32,6 @@ routes.delete('/tag/:id', tagController.deleteTag);
 // Ingredient routes
 routes.get('/ingredient', ingredientController.getIngredients);
 routes.get('/ingredient/:id', ingredientController.getIngredient);
-routes.get('/ingredient/:name', ingredientController.getIngredientByName);
 routes.post('/ingredient', ingredientController.createIngredient);
 routes.put('/ingredient/:id', ingredientController.updateIngredient);
 routes.delete('/ingredient/:id', ingredientController.deleteIngredient);
